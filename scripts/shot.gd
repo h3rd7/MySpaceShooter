eda335b3911432faa916157bc6bcc02cb83bc7da
H3rd7 <vagner_h3rd7@outlook.com>
2019-10-27 20:16:30 -0300

    created shot

---
+++ scripts/shot.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var vel = 500 # velocity
+
+func _ready():
+	set_process(true)
+
+
+func _process(delta):
+	set_position(get_position() + Vector2(0, -1) * vel * delta)
+	
+	if get_position().y < -30: # clear memory
+		print("-1 shot") # test
+		queue_free() # destroy shot
+		
