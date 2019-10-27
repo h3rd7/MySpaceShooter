be097082797ea0d825d5eae91fd3bb508b6957f1
H3rd7 <vagner_h3rd7@outlook.com>
2019-10-27 02:15:29 -0300

    added movement in spaceship

--- scripts/spaceship.gd
+++ scripts/spaceship.gd
@@ -1,9 +1,21 @@
 extends Node2D
 
+var vel = 200 # velocity
 
 func _ready():
 	set_process(true)
 	pass
 
 func _process(delta):
+	var l = 0
+	var r = 0
+
+	if Input.is_action_pressed("left_"):
+		l = -1
+	
+	if Input.is_action_pressed("right_"):
+		r = 1
+		
+	set_position(get_position() + Vector2(vel, 0) * delta * (l + r))
+	
 	pass
