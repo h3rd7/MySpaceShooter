15daea1083885b3424144cde3742c51a77daae5e
H3rd7 <vagner_h3rd7@outlook.com>
2019-10-28 16:00:03 -0300

    adding meteor

---
+++ scripts/meteor.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var vel = 250
+var rot = 0 # rotation
+
+func _ready():
+	randomize()
+	set_process(true)
+	rot = rand_range(-2, 2)
+
+func _process(delta):
+	set_position(get_position() + Vector2(0, 1) * vel * delta)
+	rotate(rot * delta)
