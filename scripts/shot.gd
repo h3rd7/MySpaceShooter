45ee60b207d6604d710861b2e4866358f808cb82
H3rd7 <vagner_h3rd7@outlook.com>
2019-10-28 01:03:19 -0300

    ship shooting implementation

--- scripts/shot.gd
+++ scripts/shot.gd
@@ -10,6 +10,6 @@ func _process(delta):
 	set_position(get_position() + Vector2(0, -1) * vel * delta)
 	
 	if get_position().y < -30: # clear memory
-		print("-1 shot") # test
+		# print("-1 shot") # test
 		queue_free() # destroy shot
 		
--- scripts/spaceship.gd
+++ scripts/spaceship.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
+var pre_shot = preload("res://scenes/shot.tscn")
 var vel = 300 # velocity
+var interval = .3
+var last_shot = 0
 
 func _ready():
 	set_process(true)
-	pass
+
 
 func _process(delta):
 	var l = 0
@@ -24,4 +27,13 @@ func _process(delta):
 	
 	set_position(get_position() + Vector2(vel, 0) * delta * (l + r))
 	
-	pass
+	if Input.is_action_pressed("shot_"): # firing
+		if last_shot <= 0:
+			# print("shoot") # test
+			var shot = pre_shot.instance()
+			shot.set_global_position(get_global_position())
+			get_node("../").add_child(shot)
+			last_shot = interval
+	
+	if last_shot > 0:
+		last_shot -= delta
